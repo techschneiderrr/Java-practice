8e1b6b2c10c5a8d8a304d6afc972fd5f4109868c
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-08-06 12:31:45 +0530

    Class 1

---
+++ hello.java
@@ -0,0 +1,15 @@
+class hello{
+    public static void main(String[] args) {
+        System.out.println("Hello world");      
+    }
+}
+
+//Class is a keyword used to declare a class in java
+//Public is an access modifier keyword in java
+//Static -> if we declare any method as static then its said to be a static method 
+//       -> there is no need to create an object to invoke the static method
+// Main() -> method is executed by the jvm so there is no need to create an object to invoke the main method as it saves memory
+//        -> represent the starting point of the program
+//Void -  its a return type, which means the method doesnt return any value and is null
+//String[] args -> is used for command line arguments
+//System.out.println -> System is a class, out is an object of PrintStream class, println is a method of the PrintStream class
