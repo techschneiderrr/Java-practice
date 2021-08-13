e7b5a6b830e8cfe279d99ab8ab91c57a4adfee2f
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-08-13 12:46:50 +0530

    Class 4

---
+++ operators.java
@@ -0,0 +1,46 @@
+public class operators {
+    public static void main(String[] args) {
+        System.out.println("All info in comments");
+    }
+    
+}
+
+// Operators : 
+// ->is used to perform operations 
+
+// types of operators : 
+// 1.Unary
+// 2.Arithmatic
+// 3.Shift
+// 4.Relational
+// 5.Bitwise
+// 6. Logical
+// 7.Ternary
+// 8.Assignment
+
+// 1.Unary :
+// ->requires only 1 operand.
+// ->used to perform various operations, incrementing/decrementing, negating, inverting value of a boolean.
+
+//  ex: public static void main(String[] args) {
+//     int x = 10;
+//     s.o.p(x++);
+//     s.o.p(++x);
+//     s.o.p(x--);
+//     s.o.p(--x);
+// }
+// Output : 10 12 12 10
+
+// public static void main(String[] args) {
+//     int a = 10;
+//     int b = 10;
+//     s.o.p(a++ + ++a); 
+//     s.o.p(b++ + b++);
+// }
+// Output : 22 21
+
+// Arithmatic : + - * / % ... 
+
+
+
+
