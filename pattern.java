fb72b5bd4e69e01812063f852cfb88a53e3e79c4
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-02 08:52:38 +0530

    class 8

--- pattern.java
+++ pattern.java
@@ -4,6 +4,7 @@ public class pattern {
     //    **
     //   ***
     //  ****    
+     public static void main(String[] args) {
         for(int i=0;i<4;i++){
             for(int j=4-i;j>1;j--){
                 System.out.print(" ");
