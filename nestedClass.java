afbc36fc788efe9b039dcd40616f6f839f9b55a4
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-06 10:46:54 +0530

    class 8

--- nestedClass.java
+++ nestedClass.java
@@ -1,7 +1,7 @@
 public class nestedClass {
     
 }
-// JAVA inner classes and nested classes
+// JAVA inner classes and nested classes 
 // ->is a class that is a class declared inside a class or an interface
 // ->we use inner classes to logically group classes and interface in one place to be more readable and maintainable
 // -> it can access all the members of tghe outer class, including the private data members and the methods
@@ -18,12 +18,20 @@ public class nestedClass {
 //--->two types:
 // 1. static
 // 2. non-static (inner class):
+
 // -->member inner class: 
 //  -->a class created within class and outside the method
 //  --> it is also known as regular inner class
 //  --> it can be declared with class modifiers like public, private and protected
+
 // -->anonymous inner class: 
 //  --> a class created for implementing an interface or extending class
 //  --> java compiler decides its name 
+//  --> is an inner class without a name  and for which only a single object is created
+//  --> it should be used if you want to override a method of a classs or interface
+//  --> can be crteated in two ways
+//     --> class (maybe abstract orconcrete)
+//     --> interface
+
 // -->local inner class: 
 //  --> a class was created within the class
